4806a66ce2adc252758d3c7fb12803fc11c8863e
taitaking <zhualonglove@gmail.com>
2018-10-10 12:46:31 +0800

    测试分支

--- src/main/java/com/example/dsd/DsdApplication.java
+++ src/main/java/com/example/dsd/DsdApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsdApplication {
 
     public static void main(String[] args) {
+
+        //测试分支功能对方是否是十分    三段
         SpringApplication.run(DsdApplication.class, args);
     }
 }
