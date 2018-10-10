18f512bd828d548f7e342e013036951d8c9ebb49
taitaking <zhualonglove@gmail.com>
2018-10-10 12:36:18 +0800

    Initial commit

---
+++ src/main/java/com/example/dsd/DsdApplication.java
@@ -0,0 +1,12 @@
+package com.example.dsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsdApplication.class, args);
+    }
+}
